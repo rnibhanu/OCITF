1f2a7f8b7e90670e7855213d563990ed48e9e0ea
Venkat <32324848+rnibhanu@users.noreply.github.com>
2021-10-28 11:18:35 +0530

    Modified to Javier ENv

--- main.tf
+++ main.tf
@@ -1,23 +1,9 @@
-terraform {
-  required_providers {
-    oci = {
-      source = "hashicorp/oci"
-    }
-  }
-}
-
-/*provider "oci" {
-  region              = "us-ashburn-2"
-  auth                = "SecurityToken"
-  config_file_profile = "OCITFTEST"
-}*/
-
 provider "oci" {
-  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaadnvhy2rcdc2xroaserm3u6m4s2bookfzc5scr23ri6nq5rzmkp4q"
-  user_ocid = "ocid1.user.oc1..aaaaaaaativfczdnl77prvyi6az7xh6ngnrfsnfq5rn3j3mjpbfcz6usc36q" 
-  private_key_path = "/var/lib/jenkins/workspace/Terraform_Pipeline/ociprivate.pem"
-  fingerprint = "b8:bf:6c:95:30:a1:6c:a7:c6:b7:68:82:a8:df:93:93"
-  region = "ap-hyderabad-1"
+  tenancy_ocid = var.tenancy_ocid
+  user_ocid = var.user_ocid
+  fingerprint = var.fingerprint
+  private_key_path = var.private_key_path
+  region = var.region
 }
 
 resource "oci_core_virtual_network" "vcn" {
